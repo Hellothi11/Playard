06032caa889e2df2ab5236c433751592986ee33c
Thi Nguyen Tam <nguyentamthi11@gmail.com>
2018-10-22 23:20:04 +0700

    add authenticate

--- api/controllers/ChatMessageController.js
+++ api/controllers/ChatMessageController.js
@@ -9,6 +9,27 @@ module.exports = {
   
     render: (request, response) => {
         return response.view('chatroom');
+    },
+
+    postMessage: async (request, response) => {
+        // Make sure this is a socket request (not traditional HTTP)
+        if (!request.isSocket) {
+            return response.badRequest();
+        }
+
+        try {
+            let user = await User.findOne({id:request.session.userId});
+            let msg = await ChatMessage.create({ message: request.body.message, createdBy: user });
+            if (!msg.id) {
+                throw new Error('Message processing failed!');
+            }
+            msg.createdBy = user;
+            ChatMessage.publishCreate(msg);
+        } catch (err) {
+            return response.serverError(err);
+        }
+
+        return response.ok();
     }
 };
 
